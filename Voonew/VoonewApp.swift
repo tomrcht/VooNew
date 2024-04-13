2a8221d641cca629c3165fe95bf40d5c9589fd06
Tom <tom.rochat@accor.com>
2024-04-13 10:45:40 +0200

    Initial Commit

---
+++ Voonew/VoonewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoonewApp.swift
+//  Voonew
+//
+//  Created by Tom on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct VoonewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoonewUITests/VoonewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VoonewUITestsLaunchTests.swift
+//  VoonewUITests
+//
+//  Created by Tom on 13/04/2024.
+//
+
+import XCTest
+
+final class VoonewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
